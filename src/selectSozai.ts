3d1dce2ee8def0fde492af41e4750e373fcd15f0
runtaku_929 <daitaku929@gmail.com>
2026-07-31 03:17:40 +0900

    Delete src directory

--- src/selectSozai.ts
+++
@@ -1,9 +0,0 @@
-import { OmitPartialGroupDMChannel, Message } from "discord.js";
-
-//< 惣菜の選択 >
-/**
- * @param message - 送信情報
- */
-export function selectSozai(message: OmitPartialGroupDMChannel<Message<boolean>>): void {
-    
-}
